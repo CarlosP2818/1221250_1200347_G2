89b6bbae51f339d53803060e87b3ca0695c22f1c
1200347 <1200347@isep.ipp.pt>
2025-10-06 18:55:15 +0100

    Add Lombok @Getter and @Setter to Author entity

--- src/main/java/pt/psoft/g1/psoftg1/authormanagement/model/Author.java
+++ src/main/java/pt/psoft/g1/psoftg1/authormanagement/model/Author.java
@@ -2,6 +2,7 @@ package pt.psoft.g1.psoftg1.authormanagement.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.StaleObjectStateException;
 import pt.psoft.g1.psoftg1.authormanagement.services.UpdateAuthorRequest;
 import pt.psoft.g1.psoftg1.exceptions.ConflictException;
@@ -9,6 +10,8 @@ import pt.psoft.g1.psoftg1.shared.model.EntityWithPhoto;
 import pt.psoft.g1.psoftg1.shared.model.Name;
 
 @Entity
+@Getter
+@Setter
 public class Author extends EntityWithPhoto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -78,4 +81,3 @@ public class Author extends EntityWithPhoto {
         return this.bio.toString();
     }
 }
-
